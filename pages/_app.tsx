a0663e93b4371c4ab53a81fa1f6a23bbe58ddbe2
ckswnskfk <ckswnskfk@hanmail.net>
2024-01-25 01:43:19 +0900

    Feat: 기본 폰트 pretendard로 수정

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,12 @@ import '@/styles/globals.css';
 
 import type { AppProps } from 'next/app';
 
+import { fontPretendard } from '@/styles/fonts';
+
 export default function App({ Component, pageProps }: AppProps) {
-	return <Component {...pageProps} />;
+	return (
+		<main className={fontPretendard.className}>
+			<Component {...pageProps} />
+		</main>
+	);
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,3 @@
-import { fontPretendard } from '@/styles/fonts';
-
 export default function Home() {
-	return (
-		<main className={fontPretendard.className}>
-			<p className='font-bold text-orange'>안녕</p>
-		</main>
-	);
+	return <div className='font-bold text-orange'>안녕</div>;
 }
