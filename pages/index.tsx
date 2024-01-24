b7d81cf8ff08e88df80b383e9cf3c47ce6548b99
ckswnskfk <ckswnskfk@hanmail.net>
2024-01-24 21:49:31 +0900

    chore: lint룰 추가, pr 템플릿 추가

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,3 @@
-import Image from 'next/image';
-import { Inter } from 'next/font/google';
-
-const inter = Inter({ subsets: ['latin'] });
-
 export default function Home() {
-	return <div className='text-xl'>안녕</div>;
+	return <div className='m-5 text-xl'>안녕</div>;
 }
