688d8112d5ef645a446b73fbdc2ac13778dc2eba
이찬주 <44164404+ckswnskfk@users.noreply.github.com>
2024-01-25 13:20:48 +0900

    feat: test 페이지, 공통 컴포넌트 추가 (#16)

---
+++ pages/test/index.tsx
@@ -0,0 +1,3 @@
+export default function Page() {
+	return <div className='font-bold text-orange'>테스트 페이지!</div>;
+}
---
+++ src/components/layout/SideMenu.tsx
@@ -0,0 +1,3 @@
+export default function SideMenu() {
+	return <div>사이드 메뉴</div>;
+}
