9bf9ae086c4e64097d1fbaf224cf485533ede439
BBoBBoM <tkrhkakt12@gmail.com>
2024-01-25 15:58:28 +0900

    Fix: Tailwind screen설정 변경 test/index 수정

--- pages/test/index.tsx
+++ pages/test/index.tsx
@@ -8,6 +8,11 @@ export default function Page() {
 		<>
 			<div className='font-bold text-orange'>테스트 페이지!</div>
 			<GeneralButton onClick={handleLoginClick}>로그인</GeneralButton>
+			<br />
+			<div className='flex justify-center gap-[10px]'>
+				<GeneralButton onClick={handleLoginClick}>수락</GeneralButton>
+				<GeneralButton onClick={handleLoginClick}>거절</GeneralButton>
+			</div>
 		</>
 	);
 }
