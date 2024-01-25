eb2fff13e957dc11c513977e0fb47d6ccc09d0dd
BBoBBoM <tkrhkakt12@gmail.com>
2024-01-25 19:36:52 +0900

    Fix: GeneralButton수정 이미지버튼 구현
    ButtonType 생성 및 type에따른 설정 적용 , prev,nex 가 type으로 넘어올시
    Button대신 Link태그로 구현

--- pages/test/index.tsx
+++ pages/test/index.tsx
@@ -1,4 +1,6 @@
-import GeneralButton from '@/src/components/ui/GeneralButton';
+import React from 'react';
+
+import GeneralButtonamend from '@/src/components/ui/GeneralButton';
 
 export default function Page() {
 	const handleLoginClick = () => {
@@ -7,12 +9,50 @@ export default function Page() {
 	return (
 		<>
 			<div className='font-bold text-orange'>테스트 페이지!</div>
-			<GeneralButton onClick={handleLoginClick}>로그인</GeneralButton>
+			<GeneralButtonamend
+				onClick={handleLoginClick}
+				buttonText='로그인'
+				buttonType='login'
+			/>
 			<br />
 			<div className='flex justify-center gap-[10px]'>
-				<GeneralButton onClick={handleLoginClick}>수락</GeneralButton>
-				<GeneralButton onClick={handleLoginClick}>거절</GeneralButton>
+				<GeneralButtonamend
+					onClick={handleLoginClick}
+					buttonText='수락'
+					buttonType='accept'
+				/>
+				<br />
+				<GeneralButtonamend
+					onClick={handleLoginClick}
+					buttonText='거절'
+					buttonType='reject'
+				/>
 			</div>
+			<br />
+			<GeneralButtonamend
+				onClick={handleLoginClick}
+				buttonText='삭제'
+				buttonType='delete'
+			/>
+			<br />
+			<div className='flex justify-center'>
+				<GeneralButtonamend
+					onClick={handleLoginClick}
+					buttonText=''
+					buttonType='prev'
+				/>
+				<GeneralButtonamend
+					onClick={handleLoginClick}
+					buttonText=''
+					buttonType='next'
+				/>
+			</div>
+			<br />
+			<GeneralButtonamend
+				onClick={handleLoginClick}
+				buttonText='대시보드 삭제하기'
+				buttonType='dashboardDelete'
+			/>
 		</>
 	);
 }
--- src/components/ui/GeneralButton.tsx
+++ src/components/ui/GeneralButton.tsx
@@ -1,58 +1,80 @@
-import React, { MouseEvent } from 'react';
+// components/Button.tsx
+import Image from 'next/image';
+import Link from 'next/link';
+import React from 'react';
 
-type event = MouseEvent<HTMLButtonElement>;
+import arrowBefore from '../../../public/icons/arrowBefore.svg';
+import arrowNext from '../../../public/icons/arrowNext.svg';
 
-interface GeneralButtonProps {
-	children: string;
-	onClick: (event: event) => void;
-	isDisabled?: boolean;
+type ButtonType =
+	| 'login'
+	| 'accept'
+	| 'reject'
+	| 'dashboardDelete'
+	| 'delete'
+	| 'prev'
+	| 'next';
+
+interface ButtonProps {
+	buttonText: string;
+	onClick: () => void;
+	isActive?: boolean;
+	buttonType: ButtonType;
 }
 
-const GeneralButton: React.FC<GeneralButtonProps> = ({
-	children,
+const GeneralButton: React.FC<ButtonProps> = ({
+	buttonText,
 	onClick,
-	isDisabled = false,
-}: GeneralButtonProps) => {
+	isActive = false,
+	buttonType,
+}: ButtonProps) => {
+	const buttonStyle = getButtonStyle(buttonType);
+
 	return (
 		<div className='flex justify-center'>
-			<button
-				className={`${getButtonStyle(children, isDisabled)}`}
-				onClick={onClick}
-				disabled={isDisabled}
-			>
-				{children}
-			</button>
+			{buttonText ? (
+				<button
+					className={`${buttonStyle} ${isActive ? 'cursor-not-allowed' : ''}`}
+					onClick={onClick}
+					disabled={isActive}
+				>
+					<span>{buttonText}</span>
+				</button>
+			) : (
+				<Link
+					className={`${buttonStyle} ${isActive ? 'cursor-not-allowed' : ''}`}
+					href='/'
+				>
+					<Image
+						// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+						src={buttonType === 'next' ? arrowNext : arrowBefore}
+						alt={`${buttonType} button`}
+						width={16}
+						height={16}
+					/>
+				</Link>
+			)}
 		</div>
 	);
 };
 
-const getButtonStyle = (children: string, isDisabled: boolean) => {
-	switch (children) {
-		case '로그인':
-			return `w-[350px] h-[50px] text-[18px] rounded-lg text-white ${
-				isDisabled
-					? 'bg-[#9FA6B2] cursor-not-allowed'
-					: 'bg-[#5534DA] cursor-pointer'
-			} lg:w-[520px]`;
-		case '수락':
-			return `w-[109px] h-7 text-[12px] rounded-[4px] bg-[#5534DA] text-white cursor-pointer
-        md:w-[72px] md:text-sm  md:h-[30px] lg:w-[84px] lg:text-sm lg:h-8`;
+const getButtonStyle = (type: ButtonType): string => {
+	const styles = {
+		login:
+			'w-[350px] h-[50px] text-[18px] rounded-lg text-white bg-[#5534DA] cursor-pointer lg:w-[520px]',
+		accept:
+			'w-[109px] h-7 text-[12px] rounded-[4px] bg-[#5534DA] text-white cursor-pointer md:w-[72px] md:text-sm  md:h-[30px] lg:w-[84px] lg:text-sm lg:h-8',
+		reject:
+			'w-[109px] h-7 text-[12px] rounded-[4px] bg-white text-[#5534DA] cursor-pointer border-[1px] border-[#D9D9D9] md:w-[72px] md:text-sm  md:h-[30px] lg:w-[84px] lg:text-sm lg:h-8',
+		dashboardDelete:
+			'w-[284px] h-[52px] text-[16px] rounded-lg cursor-pointer border-[1px] border-[#D9D9D9] lg:w-[320px] lg:h-[62px] lg:text-lg',
+		delete:
+			'w-[52px] h-7 text-sm rounded-[4px] bg-white text-[#5534DA] cursor-pointer border-[1px] border-[#D9D9D9] lg:w-[84px] lg:text-[12px]',
+		next: 'w-9 h-9 flex items-center justify-center rounded-r-[4px] border-[1px] border-[#D9D9D9] lg:w-10 lg:h-10 ',
+		prev: 'w-9 h-9 flex items-center justify-center rounded-l-[4px] border-[1px] border-[#D9D9D9] lg:w-10 lg:h-10',
+	};
 
-		case '거절':
-			return `w-[109px] h-7 text-[12px] rounded-[4px] bg-white text-[#5534DA] cursor-pointer border-[1px] border-[#D9D9D9]
-        md:w-[72px] md:text-sm  md:h-[30px] lg:w-[84px] lg:text-sm lg:h-8`;
-
-		case '삭제':
-			return `w-[52px] h-7 text-sm rounded-[4px] bg-white text-[#5534DA] cursor-pointer border-[1px] border-[#D9D9D9]
-        lg:w-[84px] lg:text-[12px]`;
-
-		case '대시보드 삭제하기':
-			return `w-[284px] h-[52px] text-[16px] rounded-lg cursor-pointer border-[1px] border-[#D9D9D9]
-        lg:w-[320px] lg:h-[62px] lg:text-lg`;
-
-		default:
-			return '';
-	}
+	return styles[type];
 };
 
 export default GeneralButton;
