018cbe4ef0a9ac25b736983eec6aebeafc3fd4bd
Lee Seo Young <seoyoung720@gmail.com>
2024-01-25 15:21:28 +0900

    feat : ModalManagerInput, ModalTitleInput 완성 - 기능은 전무함 - ModalInput에 컴포넌트 별로 구분함

--- pages/test/index.tsx
+++ pages/test/index.tsx
@@ -1,23 +1,15 @@
-import { ModalManagerInput } from '@/src/components/ui/ModalInput';
+import {
+	ModalProgressInput,
+	ModalManagerInput,
+	ModalTitleInput,
+} from '@/src/components/ui/ModalInput';
 
 export default function Page() {
 	return (
 		<>
+			<ModalProgressInput />
 			<ModalManagerInput />
-			<>
-				<div className='flex flex-col items-start gap-2.5'>
-					<label htmlFor='manager' className='text-lg'>
-						담당자
-					</label>
-					<input
-						type='text'
-						name='manager'
-						id='manager'
-						placeholder='이름을 입력해주세요'
-						className='text-lg flex w-217 h-12 p-4 items-center gap-10 border-2 rounded-md bg-white border-solid border-gray3'
-					/>
-				</div>
-			</>
+			<ModalTitleInput />
 		</>
 	);
 }
--- src/components/ui/ModalInput.tsx
+++ src/components/ui/ModalInput.tsx
@@ -1,9 +1,20 @@
 import React from 'react';
 
+function ModalProgressInput() {
+	return (
+		<div className='flex flex-col items-start gap-2.5'>
+			<label htmlFor='manager' className='text-lg font-medium'>
+				상태
+			</label>
+			<select name='progress' id='progress' />
+		</div>
+	);
+}
+
 function ModalManagerInput() {
 	return (
-		<div className='flex flex-col items-start gap-10'>
-			<label htmlFor='manager' className='text-lg'>
+		<div className='flex flex-col items-start gap-2.5'>
+			<label htmlFor='manager' className='text-lg font-medium'>
 				담당자
 			</label>
 			<input
@@ -11,10 +22,31 @@ function ModalManagerInput() {
 				name='manager'
 				id='manager'
 				placeholder='이름을 입력해주세요'
-				className='text-lg flex w-217 h-48 p-4 items-center gap-10 border-2 rounded-md bg-white border-solid border-gray3'
+				className='flex items-center h-12 gap-[10px] p-4 bg-white border-[1px] border-solid rounded-md w-[217px] border-gray3 text-base font-normal'
 			/>
 		</div>
 	);
 }
 
-export { ModalManagerInput };
+function ModalCommentInput() {
+	return <div></div>;
+}
+
+function ModalTitleInput() {
+	return (
+		<div className='inline-flex flex-col items-start gap-2.5'>
+			<label htmlFor='title' className='text-lg font-medium'>
+				제목 <span className='text-violet2'>*</span>
+			</label>
+			<input
+				type='text'
+				name='title'
+				id='title'
+				placeholder='제목을 입력해주세요'
+				className='flex w-[450px] h-12 py-[14px] px-4 items-center gap-[10px] text-base font-normal border-gray3 bg-white border-solid border-[1px] rounded-md'
+			/>
+		</div>
+	);
+}
+
+export { ModalProgressInput, ModalManagerInput, ModalTitleInput };
