40ec05bb4dd24dbccef4251ac2e25abb883f8d0e
Lee Seo Young <seoyoung720@gmail.com>
2024-01-25 14:43:02 +0900

    style : ModalManagerInput으로 분화 및 스타일 일부 적용 아직 미완성이에요.

--- pages/test/index.tsx
+++ pages/test/index.tsx
@@ -1,5 +1,23 @@
-import ModalInput from "@/src/components/ui/ModalInput";
+import { ModalManagerInput } from '@/src/components/ui/ModalInput';
 
 export default function Page() {
-	return <ModalInput/>;
+	return (
+		<>
+			<ModalManagerInput />
+			<>
+				<div className='flex flex-col items-start gap-2.5'>
+					<label htmlFor='manager' className='text-lg'>
+						담당자
+					</label>
+					<input
+						type='text'
+						name='manager'
+						id='manager'
+						placeholder='이름을 입력해주세요'
+						className='text-lg flex w-217 h-12 p-4 items-center gap-10 border-2 rounded-md bg-white border-solid border-gray3'
+					/>
+				</div>
+			</>
+		</>
+	);
 }
--- src/components/ui/ModalInput.tsx
+++ src/components/ui/ModalInput.tsx
@@ -1,3 +1,20 @@
-export default function ModalInput() {
-	return <div>ModalInput</div>;
+import React from 'react';
+
+function ModalManagerInput() {
+	return (
+		<div className='flex flex-col items-start gap-10'>
+			<label htmlFor='manager' className='text-lg'>
+				담당자
+			</label>
+			<input
+				type='text'
+				name='manager'
+				id='manager'
+				placeholder='이름을 입력해주세요'
+				className='text-lg flex w-217 h-48 p-4 items-center gap-10 border-2 rounded-md bg-white border-solid border-gray3'
+			/>
+		</div>
+	);
 }
+
+export { ModalManagerInput };
