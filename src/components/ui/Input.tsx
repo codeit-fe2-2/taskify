cacc033856ace017b5c5f3dbb36602c5a4e20633
caseV12 <caseVDev@gmail.com>
2024-01-26 14:56:11 +0900

    Feat: input 컴포넌트 비밀번호 가림/보임 버튼 구현

--- src/components/ui/Input.tsx
+++ src/components/ui/Input.tsx
@@ -18,28 +18,28 @@ const cls = (...classnames: string[]) => {
 
 const types = {
 	email: {
-		htmlType: 'email',
+		inputType: 'email',
 		label: '이메일',
 		regex: '^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{8,}$',
 		invalidMessage: '이메일 형식으로 작성해주세요',
 		placeholder: '이메일을 입력해주세요',
 	},
 	username: {
-		htmlType: 'text',
+		inputType: 'text',
 		label: '닉네임',
 		regex: '^.{0,10}$',
 		invalidMessage: '10자 이하로 작성해주세요',
 		placeholder: '닉네임을 입력해주세요',
 	},
 	password: {
-		htmlType: 'password',
+		inputType: 'password',
 		label: '비밀번호',
 		regex: '^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{8,}$',
 		invalidMessage: '영문자와 숫자를 포함한 8자 이상의 비밀번호를 입력하세요',
 		placeholder: '비밀번호를 입력해주세요',
 	},
 	'password-check': {
-		htmlType: 'password',
+		inputType: 'password',
 		label: '비밀번호 확인',
 		regex: '',
 		invalidMessage: '비밀번호가 일치하지 않습니다',
@@ -52,8 +52,9 @@ export default function Input({
 	onChange = () => {},
 	...props
 }: InputProps | PasswordCheckProps) {
-	const { htmlType, label, regex, invalidMessage, placeholder } = types[type];
+	const { inputType, label, regex, invalidMessage, placeholder } = types[type];
 	const [value, setValue] = useState('');
+	const [htmlType, setHtmlType] = useState(inputType);
 
 	const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
 		setValue(event.target.value);
@@ -65,35 +66,52 @@ export default function Input({
 			? event.target.setAttribute('data-invalid', '')
 			: event.target.removeAttribute('data-invalid');
 	};
+
+	const togglePasswordTypeOnClick = () => {
+		setHtmlType((prev) => (prev === 'password' ? 'text' : 'password'));
+	};
+
 	return (
 		<div className='flex flex-col items-start gap-2'>
 			{/* eslint-disable-next-line jsx-a11y/label-has-associated-control, jsx-a11y/label-has-for */}
-			<label htmlFor='email' className='text-base text-black2'>
+			<label htmlFor={type} className='text-base text-black2'>
 				{label}
 			</label>
-			<input
-				id={type}
-				type={htmlType}
-				value={value}
-				placeholder={placeholder}
-				onChange={handleChange}
-				onBlur={validateOnBlur}
-				required
-				pattern={
-					type !== 'password-check'
-						? regex
-						: `^${(props as PasswordCheckProps).password}$`
-				}
-				className={cls(
-					'peer flex flex-col items-center w-[52rem] h-[5rem] py-3 px-4 text-base',
-					type === 'password' ? 'text-black3' : 'text-black2',
-					'border-[1px] border-solid rounded-lg border-gray3 placeholder:text-gray4 focus:outline-none focus:border-purple data-[invalid]:border-red',
+			<div className='relative w-[520px]'>
+				<input
+					id={type}
+					type={htmlType}
+					value={value}
+					placeholder={placeholder}
+					onChange={handleChange}
+					onBlur={validateOnBlur}
+					required
+					pattern={
+						type !== 'password-check'
+							? regex
+							: `^${(props as PasswordCheckProps).password}$`
+					}
+					className={cls(
+						'peer flex flex-col items-center w-full h-[50px] py-3 px-4 text-base',
+						type === 'password' ? 'text-black3' : 'text-black2',
+						'border-[1px] border-solid rounded-lg border-gray3 placeholder:text-gray4 focus:outline-none focus:border-purple data-[invalid]:border-red',
+					)}
+				/>
+				{(type === 'password' || type === 'password-check') && (
+					<button
+						onClick={togglePasswordTypeOnClick}
+						className={cls(
+							'absolute right-4 top-[13px] size-6 border-none',
+							htmlType === 'password'
+								? 'bg-[url("/icons/visibility_off.svg")]'
+								: 'bg-[url("/icons/visibility_on.svg")]',
+						)}
+					/>
 				)}
-			></input>
-			<span className='hidden text-sm text-red peer-data-[invalid]:block'>
-				{invalidMessage}
-			</span>
-			<button></button>
+				<span className='hidden text-sm text-red peer-data-[invalid]:block'>
+					{invalidMessage}
+				</span>
+			</div>
 		</div>
 	);
 }
