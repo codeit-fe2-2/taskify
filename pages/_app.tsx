df2de11d57040f84261717bd2ee1b1e99759b0f9
ckswnskfk <ckswnskfk@hanmail.net>
2024-01-27 01:04:59 +0900

    feat: 앱에 title 추가

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,19 @@
 import '@/styles/globals.css';
 
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
 
 import { fontPretendard } from '@/styles/fonts';
 
 export default function App({ Component, pageProps }: AppProps) {
 	return (
-		<main className={fontPretendard.className}>
-			<Component {...pageProps} />
-		</main>
+		<>
+			<Head>
+				<title>Taskify</title>
+			</Head>
+			<div className={fontPretendard.className}>
+				<Component {...pageProps} />
+			</div>
+		</>
 	);
 }
