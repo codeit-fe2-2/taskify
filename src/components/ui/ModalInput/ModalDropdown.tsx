126c3d1807c49a30b74a77321eefb113adf54549
SoSoShSu <tiffany720@naver.com>
2024-01-30 16:26:06 +0900

    fix : ModalDropdown의 button이 options의 그림자에 가려지는 현상 수정 button에 하얀 배경색 설정, 그림자 투명도 오차 수정(0.8 -> 0.08)

--- src/components/ui/ModalInput/ModalDropdown.tsx
+++ src/components/ui/ModalInput/ModalDropdown.tsx
@@ -1,25 +1,25 @@
 import Image from 'next/image';
-import React, { useState } from 'react';
+import { useState } from 'react';
 
 import { inputClassNames } from './inputClassNames';
 
-type ModalDropdownType = '상태' | '담당자';
-
 interface ModalDropdownProps {
-	label: ModalDropdownType;
+	label: string;
+	options: string[]; // id와 name 모두를 받도록 설정해야 함...?
+	currentOption: string;
+	onValuesChange: (newValues: string[]) => void;
 }
 
 export default function ModalDropdown({
 	label: type,
+	options,
+	currentOption,
+	onValuesChange,
 }: ModalDropdownProps): JSX.Element {
-	let dropdownElement;
 	// chip 생기면 대대적으로 수정 예정... 임시로 string으로 대체
-	const progressOptions: string[] = ['To Do', 'On Progress', 'Done'];
+
 	const [open, setOpen] = useState<boolean>(false);
-	const [options, setOptions] = useState<string[]>(
-		type === '상태' ? progressOptions : [],
-	);
-	const [currentOption, setCurrentOption] = useState<string>(options[0] || '');
+	const [value, setValue] = useState<string>(currentOption);
 
 	const handleFocus = () => {
 		if (options.length > 0) {
@@ -27,80 +27,35 @@ export default function ModalDropdown({
 		}
 	};
 
-	const handleManagerInputChange = (
-		event: React.ChangeEvent<HTMLInputElement>,
-	) => {
-		setCurrentOption(event.target.value);
-	};
-
-	const handleAddManager = (event: React.KeyboardEvent<HTMLInputElement>) => {
-		if (event.key === 'Enter') {
-			if (
-				currentOption.trim() !== '' &&
-				!options.includes(currentOption.trim())
-			) {
-				setOptions([...options, currentOption.trim()]);
-			}
-		}
-	};
-
 	const handleOption = (option: string) => {
-		setCurrentOption(option);
+		onValuesChange([option]);
+		setValue(option);
 		setOpen(false);
 	};
 
-	switch (type) {
-		case '상태':
-			dropdownElement = (
-				<>
-					<button
-						onClick={handleFocus}
-						className='size-full rounded-md focus:z-20'
-					>
-						<p>{currentOption}</p>
-						<Image
-							src='/icons/arrow_drop_down.svg'
-							width={26}
-							height={26}
-							alt='Progress Dropdown'
-							className='absolute right-4 top-3'
-						/>
-					</button>
-				</>
-			);
-			break;
-		case '담당자':
-			dropdownElement = (
-				<>
-					<input
-						type='text'
-						name='manager'
-						id='manager'
-						placeholder='이름을 입력해주세요'
-						value={currentOption}
-						onChange={handleManagerInputChange}
-						onKeyDown={handleAddManager}
-						className='flex size-full items-center gap-[10px] rounded-md p-4 text-base font-normal outline-none'
-					/>
-					<button className='absolute right-4 top-3' onClick={handleFocus}>
-						<Image
-							src='/icons/arrow_drop_down.svg'
-							width={26}
-							height={26}
-							alt='Manager Dropdown'
-						/>
-					</button>
-				</>
-			);
-			break;
-	}
 	return (
-		<div className='inline-flex flex-col items-start gap-2.5'>
+		<div className='relative inline-flex flex-col items-start gap-2.5'>
 			<p className={`${inputClassNames.label}`}>{type}</p>
 			<div
 				className={`${inputClassNames.type.dropdown} ${inputClassNames.container}`}
 			>
-				{dropdownElement}
+				<button
+					onClick={handleFocus}
+					className='relative flex size-full items-center gap-[10px] rounded-md bg-white p-4 text-base font-normal outline-none focus:z-20'
+				>
+					{value ? (
+						<p>{value}</p>
+					) : (
+						<p className='text-gray4'>이름을 입력해주세요</p>
+					)}
+					<Image
+						src='/icons/arrow_drop_down.svg'
+						width={26}
+						height={26}
+						alt='Progress Dropdown'
+						className='absolute right-4 top-3'
+					/>
+				</button>
 				{open && (
 					<ul className={`${inputClassNames.dropdownOptions}`}>
 						{options.map((option, index) => (
@@ -109,7 +64,7 @@ export default function ModalDropdown({
 									className='flex w-full flex-row gap-1.5 px-2 py-[13px]'
 									onClick={() => handleOption(option)}
 								>
-									{currentOption === option ? (
+									{value === option ? (
 										<Image
 											src='/icons/check.svg'
 											width={22}
