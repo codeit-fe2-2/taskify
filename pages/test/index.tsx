e86225b02d178b9cf3954bde0d796519ee3c1602
Lee Seo Young <seoyoung720@gmail.com>
2024-02-01 01:49:09 +0900

    test : 테스트용 페이지 작성

--- pages/test/index.tsx
+++ pages/test/index.tsx
@@ -1,4 +1,9 @@
+import { useState } from 'react';
+
 import CopyDropdown from '@/src/components/ui/ModalInput/CopyDropdown';
+import ModalImage from '@/src/components/ui/ModalInput/ModalImage';
+import ModalInput from '@/src/components/ui/ModalInput/ModalInput';
+import ModalTextarea from '@/src/components/ui/ModalInput/ModalTextarea';
 
 import mockupData from './mockup.json';
 
@@ -20,14 +25,103 @@ export default function Page() {
 	const currentColumn = columnsData.find(
 		(column) => column.id === currentColumnId,
 	);
+
+	const [columnId, setColumnId] = useState<number | null>(currentColumnId);
+	const [assigneeUserId, setAssigneeUserId] = useState<number | null>();
+	const [title, setTitle] = useState<string>('');
+	const [description, setDescription] = useState<string>('');
+	const [dueDate, setDueDate] = useState<string | null>();
+	const [tags, setTags] = useState<string[] | null>();
+	const [imageUrl, setImageUrl] = useState<string | null>();
+
+	const createCardData = {
+		assigneeUserId: assigneeUserId,
+		dashboardId: 0,
+		columnId: columnId,
+		title: title,
+		description: description,
+		dueDate: dueDate,
+		tags: tags,
+		imageUrl: imageUrl,
+	};
+
+	const editCardData = {
+		columnId: columnId,
+		assigneeUserId: assigneeUserId,
+		title: title,
+		description: description,
+		dueDate: dueDate,
+		tags: tags,
+		imageUrl: imageUrl,
+	};
+
+	const handleColumnId = (value: number | null) => {
+		setColumnId(value);
+	};
+
+	const handleAssigneeUserId = (value: number | null) => {
+		setAssigneeUserId(value);
+	};
+
+	const handleTitle = (value: string[]) => {
+		setTitle(value[0]);
+	};
+
+	const handleDescription = (value: string) => {
+		setDescription(value);
+	};
+
+	const handleDueDate = (value: string[] | null) => {
+		if (value) {
+			setDueDate(value[0]);
+		} else {
+			setDueDate(null);
+		}
+	};
+
+	const handleTags = (value: string[] | null) => {
+		setTags(value);
+	};
+
+	const handleImageUrl = (value: string | null) => {
+		setImageUrl(value);
+	};
+
+	const handleCreateCard = () => {
+		console.log(createCardData);
+	};
+
+	const handleEditCard = () => {
+		console.log(editCardData);
+	};
+
 	return (
-		<div className='flex flex-row gap-x-3 p-3'>
-			<CopyDropdown
-				label='상태'
-				data={columnsData}
-				currentData={currentColumn}
+		<div className='flex flex-col gap-y-3 p-3'>
+			<div className='flex flex-row gap-x-3'>
+				<CopyDropdown
+					label='상태'
+					data={columnsData}
+					currentData={currentColumn}
+					onDropdownSelect={handleColumnId}
+				/>
+				<CopyDropdown
+					label='담당자'
+					data={membersData}
+					onDropdownSelect={handleAssigneeUserId}
+				/>
+			</div>
+			<ModalInput label='제목' required onValueChange={handleTitle} />
+			<ModalTextarea
+				label='설명'
+				required={true}
+				isButton={false}
+				onTextChange={handleDescription}
 			/>
-			<CopyDropdown label='담당자' data={membersData} />
+			<ModalInput label='마감일' onValueChange={handleDueDate} />
+			<ModalInput label='태그' onValueChange={handleTags} />
+			<ModalImage label='이미지' onImageSelect={handleImageUrl} />
+			<button onClick={handleCreateCard}>createCardData</button>
+			<button onClick={handleEditCard}>editCardData</button>
 		</div>
 	);
 }
