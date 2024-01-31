cb3c69a9113602e10bc00749a6aedb451517a79e
Lee Seo Young <seoyoung720@gmail.com>
2024-02-01 01:24:31 +0900

    feat : ModalImage가 라벨도 취급하도록 수정

--- src/components/ui/ModalInput/ModalImage.tsx
+++ src/components/ui/ModalInput/ModalImage.tsx
@@ -2,10 +2,14 @@ import Image from 'next/image';
 import { ChangeEvent, useState } from 'react';
 
 interface ModalImageProps {
+	label: string;
 	onImageSelect: (imageUrl: string | null) => void;
 }
 
-export default function ModalImage({ onImageSelect }: ModalImageProps): JSX.Element {
+export default function ModalImage({
+	label,
+	onImageSelect,
+}: ModalImageProps): JSX.Element {
 	const [backgroundImage, setBackgroundImage] = useState<string | null>(null);
 
 	const handleFileInputClick = () => {
@@ -30,38 +34,43 @@ export default function ModalImage({ onImageSelect }: ModalImageProps): JSX.Elem
 	};
 
 	return (
-		<button
-			onClick={handleFileInputClick}
-			className='group relative flex size-[76px] shrink-0 items-center justify-center rounded-md bg-white'
-		>
-			{backgroundImage ? (
-				<>
-					<Image
-						src={backgroundImage}
-						fill={true}
-						alt='test Image'
-						className='rounded-md'
-					/>
-					<div className='z-10 flex size-[76px] items-center justify-center rounded-md bg-black4 opacity-0 group-hover:opacity-60'>
+		<div className='inline-flex flex-col items-start gap-2.5'>
+			<label htmlFor='comment' className='text-lg font-medium'>
+				{label}
+			</label>
+			<button
+				onClick={handleFileInputClick}
+				className='group relative flex size-[76px] shrink-0 items-center justify-center rounded-md bg-white'
+			>
+				{backgroundImage ? (
+					<>
 						<Image
-							src='/icons/edit.svg'
-							width={30}
-							height={30}
-							alt='Edit Image Icon'
+							src={backgroundImage}
+							fill={true}
+							alt='test Image'
+							className='rounded-md'
 						/>
+						<div className='z-10 flex size-[76px] items-center justify-center rounded-md bg-black4 opacity-0 group-hover:opacity-60'>
+							<Image
+								src='/icons/edit.svg'
+								width={30}
+								height={30}
+								alt='Edit Image Icon'
+							/>
+						</div>
+					</>
+				) : (
+					<div className='rounded-md bg-gray3 p-6'>
+						<div className='size-[28px] bg-violet2' />
 					</div>
-				</>
-			) : (
-				<div className='rounded-md bg-gray3 p-6'>
-					<div className='size-[28px] bg-violet2' />
-				</div>
-			)}
-			<input
-				type='file'
-				id='fileInput'
-				className='hidden'
-				onChange={handleFileChange}
-			/>
-		</button>
+				)}
+				<input
+					type='file'
+					id='fileInput'
+					className='hidden'
+					onChange={handleFileChange}
+				/>
+			</button>
+		</div>
 	);
 }
