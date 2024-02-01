9576ee91d2e649347ee1e9b7bf2a82469f986a4a
Sungeunji <danceunji@gmail.com>
2024-02-01 15:12:37 +0900

    Refactor: ColorTagChip에서 %를 사용해서  로직을 변경

--- pages/test/index.tsx
+++ pages/test/index.tsx
@@ -1,29 +1,10 @@
 import React from 'react';
 
-import IconButton from '@/src/components/ui/Button/IconButton';
-
-import {
-	ModalCommentInput,
-	ModalDeadlineInput,
-	ModalImageInput,
-	ModalManagerInput,
-	ModalProgressInput,
-	ModalTagInput,
-	ModalTitleInput,
-} from '@/src/components/ui/ModalInput';
-
 import Chips from '@/src/components/ui/Chips/Chips';
 
 export default function Page() {
 	return (
 		<>
-			<ModalProgressInput />
-			<ModalManagerInput />
-			<ModalCommentInput />
-			<ModalTitleInput />
-			<ModalDeadlineInput />
-			<ModalTagInput />
-			<ModalImageInput />
 			<Chips />
 		</>
 	);
