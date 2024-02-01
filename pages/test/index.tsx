3a9f48d0ba4d51e57769c330f8fd1550b5168890
Sungeunji <danceunji@gmail.com>
2024-02-01 11:06:35 +0900

    Feat: Card 작성중..

--- pages/test/index.tsx
+++ pages/test/index.tsx
@@ -1,30 +1,11 @@
 import React from 'react';
 
-import IconButton from '@/src/components/ui/Button/IconButton';
-
-import {
-	ModalCommentInput,
-	ModalDeadlineInput,
-	ModalImageInput,
-	ModalManagerInput,
-	ModalProgressInput,
-	ModalTagInput,
-	ModalTitleInput,
-} from '@/src/components/ui/ModalInput';
-
-import Chips from '@/src/components/ui/Chips/Chips';
+import Card from '@/src/components/ui/Card';
 
 export default function Page() {
 	return (
 		<>
-			<ModalProgressInput />
-			<ModalManagerInput />
-			<ModalCommentInput />
-			<ModalTitleInput />
-			<ModalDeadlineInput />
-			<ModalTagInput />
-			<ModalImageInput />
-			<Chips />
+			<Card />
 		</>
 	);
 }
--- src/components/ui/Card.tsx
+++ src/components/ui/Card.tsx
@@ -1,3 +1,76 @@
+import Image from 'next/image';
+
+import ColorTagChip from './Chips/ColorTagChip';
+
 export default function Card() {
-	return <div>Card</div>;
+	return (
+		<div>
+			<div className='m-5 h-[125px] w-[314px] rounded-[6px] border-[1px] border-solid border-gray3 '>
+				<div className='m-[10px] ml-[20px] flex h-[51px] w-[161px] flex-col gap-[10px]'>
+					<div className='text-[16px] text-black2'>새로운 일정관리</div>
+					<ColorTagChip
+						textColor='text-sortTextGreen'
+						bgColor='bg-sortTextBgGreen'
+						fontSize='text-xs'
+					>
+						할일
+					</ColorTagChip>
+				</div>
+
+				<div className='ml-[20px] mt-5 flex h-[24px] w-[247px] items-center justify-between gap-[6px]'>
+					<div>
+						<div className='flex gap-[6px]'>
+							<Image
+								src='/icons/calendar.svg'
+								width={18}
+								height={18}
+								alt='calender icon'
+							/>
+							<div className='text-gray5'>마감일</div>
+						</div>
+					</div>
+					<div>배지</div>
+				</div>
+			</div>
+
+			{/* without image */}
+			<div className='m-5  h-[297px] w-[314px] rounded-[6px] border-[1px] border-solid border-gray3'>
+				<div className='ml-2 mt-5'>
+					<Image
+						src='/images/card_image1.png'
+						width='274'
+						height='160'
+						alt='example photo'
+						className='m-[10px]'
+					/>
+				</div>
+
+				<div className='ml-[20px] mt-[12px] flex h-[51px] w-[161px] flex-col gap-[10px]'>
+					<div className='text-[16px] text-black2'>새로운 일정관리</div>
+					<ColorTagChip
+						textColor='text-sortTextGreen'
+						bgColor='bg-sortTextBgGreen'
+						fontSize='text-xs'
+					>
+						할일
+					</ColorTagChip>
+				</div>
+
+				<div className='ml-[20px] mt-5 flex h-[24px] w-[247px] items-center justify-between gap-[6px]'>
+					<div>
+						<div className='flex gap-[6px]'>
+							<Image
+								src='/icons/calendar.svg'
+								width={18}
+								height={18}
+								alt='calender icon'
+							/>
+							<div className='text-gray5'>마감일</div>
+						</div>
+					</div>
+					<div>배지</div>
+				</div>
+			</div>
+		</div>
+	);
 }
