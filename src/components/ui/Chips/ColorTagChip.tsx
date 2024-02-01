a3eb393fd0a9365d9fd68c2db01c90ef99c14379
성은지 <116065415+Nebaisgood@users.noreply.github.com>
2024-02-01 16:28:02 +0900

    Feature/eunji/10 (#39)
    * Feat: Chips 수정중 #10
    
    * etc: 컴포넌트 분할
    
    * Refector: 컴포넌트 구성 및 네이밍 수정
    
    * Refactor: Size 타입 AddButton 에서서 Chips로 이동. ColorDotButton 이름수정
    
    * Feat: ColorDotButton에서 흰색 체크 아이콘을 추가, 크기에 따라 위치 맞도록 설정
    
    * Feat: ColorTagChip 컴포넌트에서 유저가 입력하는 글자 수에 따라 5개의 색이 정해지는 기능을 추가
    
    * Refactor: 불필요한 폴더 삭제
    
    * Refactor: ColorTagChip에서 %를 사용해서  로직을 변경
    
    * Refactor: ColorTagChip에서 %를 사용해서  로직을 변경
    
    * Refactor: ColorTagChip에서 %를 사용해서  로직을 변경

---
+++ src/components/ui/Chips/ColorTagChip.tsx
@@ -0,0 +1,31 @@
+export default function ColorTags({
+	children,
+	fontSize,
+}: {
+	children: React.ReactNode;
+	fontSize: string;
+}): React.JSX.Element {
+	const tagColorSet = [
+		{ textColor: 'text-sortTextGreen', bgColor: 'bg-sortTextBgGreen' },
+		{ textColor: 'text-sortTextPink', bgColor: 'bg-sortTextBgPink' },
+		{ textColor: 'text-sortTextBlue', bgColor: 'bg-sortTextBgBlue' },
+		{ textColor: 'text-sortTextOrange', bgColor: 'bg-sortTextBgOrange' },
+		{ textColor: 'text-violet2', bgColor: 'bg-violet1' },
+	];
+
+	const textLength = typeof children === 'string' ? children.length : 1;
+
+	const colorIndex = textLength % 5;
+
+	const { textColor, bgColor } = tagColorSet[colorIndex];
+
+	return (
+		<div>
+			<button
+				className={`${textColor} ${bgColor} ${fontSize < '12' ? 'text-xs' : 'text-base'} rounded-[4px] px-[6px] py-[4px] text-sm`}
+			>
+				{children}
+			</button>
+		</div>
+	);
+}
