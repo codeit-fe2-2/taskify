e4c4f2f46cd16f0d885222e531b671407bf6db44
Sungeunji <danceunji@gmail.com>
2024-02-04 11:27:44 +0900

    Refactor:ColorDotButton 수정중

--- src/components/ui/ColorDotButton.tsx
+++ src/components/ui/ColorDotButton.tsx
@@ -1,22 +1,30 @@
 import Image from 'next/image';
+import { useState } from 'react';
 export default function ColorDotButtons({
 	bgColor,
 	height,
 	width,
 }: {
-	bgColor: string;
+	bgColor: 'bg-green' | 'bg-green' |
 	height: string;
 	width: string;
 }): React.JSX.Element {
+	const [isClicked, setIsClicked] = useState(false);
+
 	const imageSize = {
 		left: `${width === 'w-8' ? 6 : 2.5}px`,
 		top: `${height === 'h-8' ? 5 : 1.5}px`,
+	};
+
+	const handleClick = () => {
+		setIsClicked(true);
 	};
 
 	return (
 		<div className='relative'>
 			<button
 				className={` rounded-full ${bgColor} ${height} ${width}`}
+				onClick={handleClick}
 			></button>
 			<div className='absolute ' style={imageSize}>
 				<Image
