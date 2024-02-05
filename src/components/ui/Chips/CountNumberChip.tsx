761bdf3df147dba31b2d68c88e240943fb5fb717
Sungeunji <danceunji@gmail.com>
2024-02-05 13:53:22 +0900

    Feat: CountNumber 컴포넌트를 사용시 대쉬보드 추가될 때마다 숫자값이 상승하는 기능 구현

--- src/components/ui/Chips/CountNumberChip.tsx
+++ src/components/ui/Chips/CountNumberChip.tsx
@@ -1,12 +1,26 @@
+import { useEffect, useState } from 'react';
+
+interface CountNumberChipProps {
+	initialValue: number;
+	dashboardAdded: boolean;
+}
+
 export default function CountNumberChip({
-	children,
-}: {
-	children: React.ReactNode;
-}): React.JSX.Element {
+	initialValue,
+	dashboardAdded,
+}: CountNumberChipProps): React.ReactElement {
+	const [count, setCount] = useState(initialValue);
+
+	useEffect(() => {
+		if (dashboardAdded) {
+			setCount((prevCount) => prevCount + 1);
+		}
+	});
+
 	return (
 		<div>
 			<div className='flex w-6 justify-center gap-10 rounded-[4px] bg-gray2 px-[6px] py-[3px] align-middle text-gray5'>
-				{children}
+				{count}
 			</div>
 		</div>
 	);
