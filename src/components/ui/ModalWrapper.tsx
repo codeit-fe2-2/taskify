c4d394fdce4cb85a34c7428df9ea9d1673615c48
이찬주 <44164404+ckswnskfk@users.noreply.github.com>
2024-02-05 20:54:51 +0900

    Feature: 모달 시스템 및 공통 모달 구현 (#60)
    * feat: 공통 모달 컴포넌트 구현
    
    * feat: 모달 provider 구현
    
    * feat: 단순 텍스트, 버튼 모달 구현
    
    * feat: ModalProvider 앱 전체에 적용
    
    * feat: ConfirmModal 구현

---
+++ src/components/ui/ModalWrapper.tsx
@@ -0,0 +1,49 @@
+import { ReactNode, useEffect, useRef } from 'react';
+
+interface Props {
+	id: string;
+	children: ReactNode;
+	onRemove: (id: string) => void;
+}
+
+function ModalWrapper({ children, id, onRemove }: Props) {
+	const ref = useRef<HTMLDialogElement>(null);
+
+	const handleClickOutside = (e: MouseEvent) => {
+		if (ref.current && ref.current === e.target) {
+			onRemove(id);
+		}
+	};
+
+	const handleKeydownEsc = (e: KeyboardEvent) => {
+		if (e.keyCode === 27) {
+			onRemove(id);
+		}
+	};
+
+	useEffect(() => {
+		if (ref.current) {
+			ref.current.showModal();
+			document.addEventListener('keydown', handleKeydownEsc);
+			document.addEventListener('click', handleClickOutside);
+			document.body.style.overflow = 'hidden';
+		}
+
+		return () => {
+			document.removeEventListener('keydown', handleKeydownEsc);
+			document.removeEventListener('click', handleClickOutside);
+			document.body.style.overflow = ' auto';
+			if (ref.current) {
+				ref.current.close();
+			}
+		};
+	}, [id]);
+
+	return (
+		<dialog ref={ref} className='rounded-lg'>
+			{children}
+		</dialog>
+	);
+}
+
+export default ModalWrapper;
