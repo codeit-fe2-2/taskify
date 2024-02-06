81197964dd3aeefebedadb69c717484fbbcbad6d
Sungeunji <danceunji@gmail.com>
2024-02-06 19:54:16 +0900

    Feat: Card 컴포넌트 작성 및 반응형구현중

--- src/components/ui/Card.tsx
+++ src/components/ui/Card.tsx
@@ -1,67 +1,67 @@
 import Image from 'next/image';
 
-// import { ReactNode } from 'react';
+import AddButton from './AddButton';
 import ColorTagChip from './Chips/ColorTagChip';
 
 export default function Card() {
 	return (
-		<div>
-			<div className=' m-5 flex h-[125px] w-[314px] flex-col items-start justify-center gap-2 rounded-[6px] border-[1px] border-solid border-gray3 pl-[10px]'>
-				<div className=' flex h-[21px] w-[161px] text-[16px] text-black2'>
-					새로운 일정관리
-				</div>
-				<div className='flex gap-2'>
-					<ColorTagChip>할일</ColorTagChip>
-					<ColorTagChip>프로젝트</ColorTagChip>
-					<ColorTagChip>프론트엔드</ColorTagChip>
-				</div>
-
-				<div className=' flex h-[24px] w-[247px] items-center justify-between gap-[6px]'>
-					<div>
-						<div className='flex gap-[6px]'>
-							<Image
-								src='/icons/calendar.svg'
-								width={18}
-								height={18}
-								alt='calender icon'
-							/>
-							<div className='text-gray5'>마감일</div>
-						</div>
-					</div>
-					<div>배지</div>
+		<div className='flex flex-col gap-4 sm:w-[308px] md:w-[584px] lg:w-[354px] '>
+			<div className='flex h-8 justify-center rounded-[6px] border-[1px] border-solid border-gray3'>
+				<div className='border-[1px] border-solid border-gray3'>
+					<AddButton
+						handleCreateNew={() => {
+							console.log('clicked');
+						}}
+					/>
 				</div>
 			</div>
 
-			{/* with image */}
-			<div className='m-5 h-[297px] w-[314px] rounded-[6px] border-[1px] border-solid border-gray3'>
-				<div className='ml-2 mt-5'>
-					<Image
-						src='/images/card_image1.png'
-						width='274'
-						height='160'
-						alt='example photo'
-						className='m-[10px]'
-					/>
-				</div>
+			<div className='flex flex-col gap-[12px] rounded-[6px] border-[1px] border-solid border-gray3 p-5 md:flex-row'>
+				<Image
+					src='/images/card_image1.png'
+					width='274'
+					height='160'
+					alt='사진 예시'
+					className='w-full  sm:w-[260] md:w-[90px] lg:w-[274]'
+				/>
+				<div className='flex flex-col gap-[6px] md:items-start md:justify-center'>
+					<div className='h-[21px] text-[16px] text-black2 sm:text-[14px]'>
+						title: strigdddd
+					</div>
+					<div className='md:flex '>
+						<div className='flex w-full gap-2 md:flex-row md:gap-[10px]'>
+							<ColorTagChip>상</ColorTagChip>
+							<ColorTagChip>프로젝트</ColorTagChip>
+							<ColorTagChip>프론트엔드</ColorTagChip>
+							{/* tags:['string'] */}
+						</div>
 
-				<div className='ml-[20px] mt-[12px] flex h-[51px] w-[161px] flex-col gap-[10px]'>
-					<div className='text-[16px] text-black2'>새로운 일정관리</div>
-					<ColorTagChip>할일</ColorTagChip>
-				</div>
+						<div className=' flex w-auto items-center justify-between gap-[160px]'>
+							<div className='flex items-center justify-center gap-[6px]'>
+								{/* assignee */}
+								<Image
+									src='/icons/calendar.svg'
+									width={18}
+									height={18}
+									alt='캘린더 아이콘'
+									className='sm:size-[14px] lg:size-[18px]'
+								/>
+								<div className='text-[12px] text-gray5 sm:text-[10px]'>
+									2022.12.31
+								</div>
+								{/* dueDate:"string" */}
+							</div>
 
-				<div className='ml-[20px] mt-5 flex h-[24px] w-[247px] items-center justify-between gap-[6px]'>
-					<div>
-						<div className='flex gap-[6px]'>
-							<Image
-								src='/icons/calendar.svg'
-								width={18}
-								height={18}
-								alt='calender icon'
-							/>
-							<div className='text-gray5'>마감일</div>
+							<div className='size-6 rounded-full bg-green px-[8px] text-[12px] '>
+								p
+							</div>
+							{/* "profileImageUrl": "string" */}
+
+							{/* assignee:{ "profileImageUrl": "string",
+        "nickname": "string",
+        "id": 0} */}
 						</div>
 					</div>
-					<div>배지</div>
 				</div>
 			</div>
 		</div>
