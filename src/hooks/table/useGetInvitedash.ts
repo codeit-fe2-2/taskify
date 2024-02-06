5324af3854a954c0f411889ca617f4356266caad
Lee Seo Young <seoyoung720@gmail.com>
2024-02-06 13:46:38 +0900

    fix : 대시보드 여러번 출력되는 현상 수정

--- src/hooks/table/useGetInvitedash.ts
+++ src/hooks/table/useGetInvitedash.ts
@@ -4,13 +4,12 @@ import { InviteDashGetResponse } from '@/src/types/table';
 
 export const useGetInviteDash = (title: string) => {
 	const getInviteDash = () => {
-		// title 값이 있으면 해당 쿼리 파라미터를 포함한 URL을 생성
 		const url = title ? `invitations?title=${title}` : 'invitations';
 
 		return axiosInstance.get<InviteDashGetResponse>(url);
 	};
 
-	const { data, execute } = useAsync(getInviteDash);
+	const { data, execute } = useAsync(getInviteDash, true);
 
 	return { inviteDashInfo: data, execute };
 };
