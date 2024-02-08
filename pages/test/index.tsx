bf65bf9c22a15647e9e252e0d8516c3d6bf81942
Lee Seo Young <seoyoung720@gmail.com>
2024-02-08 16:20:22 +0900

    fix : email 대신 nickname을 사용하도록 수정함 + email.length 대신에 charCodeAt을 사용해 첫 글자의 유니코드 값으로 hash 값을 구하도록 하여 색의 중복을 최대한 피하게 유도함

--- pages/test/index.tsx
+++ pages/test/index.tsx
@@ -3,10 +3,13 @@ import React from 'react';
 import DefaultProfileImage from '@/src/components/ui/DefaultProfileImage';
 
 export default function Page() {
-	const email = 'example@example.com';
+	const nickname = '각별님 너무 좋아';
 	return (
 		<div className='flex size-full flex-col gap-6 bg-violet2 p-1'>
-			<DefaultProfileImage email={email} classNames='size-[96px] text-[16px]' />
+			<DefaultProfileImage
+				nickname={nickname}
+				classNames='size-[96px] text-[16px]'
+			/>
 		</div>
 	);
 }
--- src/components/ui/DefaultProfileImage.tsx
+++ src/components/ui/DefaultProfileImage.tsx
@@ -1,19 +1,18 @@
 import React from 'react';
 
 interface DefaultProfileImageProps {
-	email: string;
+	nickname: string;
 	classNames: string;
 }
 
 export default function DefaultProfileImage({
-	email,
+	nickname,
 	classNames, // 텍스트 사이즈와 div 크기는 쓸 때마다 직접 기입 할 것
 }: DefaultProfileImageProps): JSX.Element {
 	const randomColors = [
 		'bg-[#fcdB00]',
 		'bg-[#1c1917]',
 		'bg-[#a3c4a2]',
-		'bg-[#f4d7da]',
 		'bg-[#c4b1a2]',
 		'bg-[#d25b68]',
 		'bg-[#9dd7ed]',
@@ -21,14 +20,17 @@ export default function DefaultProfileImage({
 		'bg-[#ffc85a]',
 		'bg-[#25e09a]',
 	];
-	const emailHash = email.length % randomColors.length;
+	const nicknameHash = nickname.charCodeAt(0) % randomColors.length;
 	return (
 		<>
 			<div
-				className={`${classNames} ${randomColors[emailHash]} flex items-center justify-center rounded-full bg-red font-semibold text-white outline outline-4 outline-offset-0 outline-white`}
+				className={`${classNames} ${randomColors[nicknameHash]} flex items-center justify-center rounded-full bg-red font-semibold text-white outline outline-4 outline-offset-0 outline-white`}
 			>
-				<p>{email.charAt(0).toUpperCase()}</p>
+				<p>{nickname.charAt(0).toUpperCase()}</p>
 			</div>
+			<p>
+				{nickname}, {nicknameHash}, {nickname.charCodeAt(0)}
+			</p>
 		</>
 	);
 }
