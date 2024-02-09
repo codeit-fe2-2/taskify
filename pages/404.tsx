9f9eee40f1f6ffb76a74493d5fb52b5ffce99d5c
Sungeunji <danceunji@gmail.com>
2024-02-09 20:09:57 +0900

    Feat: 404페이지 초안 작성중1

--- pages/404.tsx
+++ pages/404.tsx
@@ -3,11 +3,27 @@ import Link from 'next/link';
 
 export default function Custom404() {
 	return (
-		<div className='flex w-full flex-col items-center text-[100px] font-bold text-purple'>
-			<h1>자,</h1>
-			<Image src='/images/404Bill.webp' width={1000} height={920} alt='404' />
+		<div className='mt-[45px] flex w-full flex-col items-center text-[50px] font-medium '>
+			<h1>404</h1>
+			<div className='relative'>
+				<Image
+					src='/images/404Bill.webp'
+					width={800}
+					height={720}
+					alt='빌게이츠'
+					className='shadow-lg'
+				/>
+				{/* <div className='absolute bottom-[75px] right-8 h-[180px] w-[250px] rounded-[10px] bg-gray2 text-[40px] sm:bottom-[130px] sm:text-[30px]'>
+					<span>없는 페이지</span>
+				</div> */}
+			</div>
+
 			<p>
-				<Link href='/'>홈으로 돌아가자</Link>
+				<button className='mt-4 w-[800px] rounded-[20px] border-[1px] border-solid p-2 text-[40px] hover:bg-sortTextBgBlue'>
+					<Link href='/' className=''>
+						돌아가기
+					</Link>
+				</button>
 			</p>
 		</div>
 	);
