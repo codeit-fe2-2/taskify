7a7310a4043a88cce6f21284efedfd3628a246c3
Lee Seo Young <seoyoung720@gmail.com>
2024-02-10 11:49:48 +0900

    refactor : ModalInput 태그에 ColorTagChip 반영

--- pages/test/index.tsx
+++ pages/test/index.tsx
@@ -1,13 +1,12 @@
 import React, { useState } from 'react';
 
-import DefaultProfileImage from '@/src/components/ui/DefaultProfileImage';
+import ColorTagChip from '@/src/components/ui/Chips/ColorTagChip';
 import ModalDropdown from '@/src/components/ui/ModalInput/ModalDropdown';
+import ModalInput from '@/src/components/ui/ModalInput/ModalInput';
 
 import mockupData from './DropdownMockup.json';
 
 export default function Page() {
-	const nickname = '각별님 너무 좋아';
-
 	const managersData = mockupData.members;
 	const [selectedManagerId, setSelectedManagerId] = useState<number>();
 	const columnsData = mockupData.data;
@@ -18,12 +17,18 @@ export default function Page() {
 		console.log(selectedColumnId);
 	};
 
+	const [currentTags, setCurrentTags] = useState<string[]>([]);
+
+	const handleTag = () => {
+		console.log('tag!');
+	};
+
+	const handleTags = () => {
+		console.log(currentTags);
+	};
+
 	return (
 		<div className='flex size-full flex-col gap-6 bg-gray2 p-1'>
-			<DefaultProfileImage
-				nickname={nickname}
-				classNames='size-[96px] text-[16px]'
-			/>
 			<ModalDropdown
 				label='담당자'
 				data={managersData}
@@ -41,7 +46,13 @@ export default function Page() {
 				currentData={columnsData[0]}
 				onDropdownSelect={(columnId) => setSelectedColumnId(columnId)}
 			/>
-			<button onClick={handleValues}>버튼</button>
+			<button onClick={handleValues}>드롭다운 버튼</button>
+			<ColorTagChip onTagClick={handleTag}>각별님영상올려</ColorTagChip>
+			<ModalInput
+				label='태그'
+				onValueChange={(newValues) => setCurrentTags(newValues)}
+			/>
+			<button onClick={handleTags}>태그 버튼</button>
 		</div>
 	);
 }
