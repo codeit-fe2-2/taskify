3a0c87ec40443faed303f7b110fb0df747fb7d24
Lee Seo Young <seoyoung720@gmail.com>
2024-02-12 04:20:36 +0900

    feat : CardDetails.tsx 작성 도중 임시 저장 - Get 완비 - Style 완비

--- pages/test/index.tsx
+++ pages/test/index.tsx
@@ -1,19 +1,9 @@
 import BasicLayout from '@/src/components/layout/BasicLayout';
 import CardDetails from '@/src/components/ui/Modal/CardDetails';
-import { useModal } from '@/src/contexts/ModalProvider';
-
 export default function MyDashboardPage() {
-	const { openModal, closeModal } = useModal();
-	const modalId = crypto.randomUUID();
-	const cardId = 2742;
-	const handleClickOpenModal = () => {
-		openModal(<CardDetails cardId={cardId} />, modalId);
-	};
 	return (
 		<BasicLayout>
-			<button className='bg-pink' onClick={handleClickOpenModal}>
-				모달 얍
-			</button>
+			<CardDetails cardId={2784} />
 		</BasicLayout>
 	);
 }
