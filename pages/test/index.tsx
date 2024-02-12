8f9ca9031f04b0b588888bfa3f2e26b94313512c
TaciTa <79896328+00TaciTa00@users.noreply.github.com>
2024-02-12 15:11:23 +0900

    Feat : CardDetail 모달 임시 저장 (#100)
    * feat : CardDetails.tsx 작성 도중 임시 저장
    - Get 완비
    - Style 완비
    
    * fix : 머지하면서 사라진 hook 복구
    
    ---------
    
    Co-authored-by: Lee Seo Young <seoyoung720@gmail.com>
    Co-authored-by: 이찬주 <44164404+ckswnskfk@users.noreply.github.com>

--- pages/test/index.tsx
+++ pages/test/index.tsx
@@ -1,39 +1,11 @@
 import { useState } from 'react';
 
 import CardDetails from '@/src/components/ui/Modal/CardDetails';
-import TodoModal from '@/src/components/ui/Modal/TodoModal';
 
-export default function Page() {
-	const [open, setOpen] = useState(false);
-	const handleOpen = () => {
-		setOpen(true);
-		console.log('open');
-	};
-	const handleClose = () => {
-		setOpen(false);
-	};
+export default function MyDashboardPage() {
 	return (
-		<div>
-			<button onClick={handleOpen}>click</button>
-			{open && (
-				<TodoModal
-					onClose={handleClose}
-					handleSubmit={() => {
-						console.log('submit');
-					}}
-					mode='생성'
-					postData={{
-						assigneeUserId: 765,
-						dashboardId: 3226,
-						columnId: 10771,
-						title: '',
-						description: '',
-						dueDate: '',
-						tags: [],
-						imageUrl: '',
-					}}
-				/>
-			)}
-		</div>
+		<BasicLayout>
+			<CardDetails cardId={2784} />
+		</BasicLayout>
 	);
 }
